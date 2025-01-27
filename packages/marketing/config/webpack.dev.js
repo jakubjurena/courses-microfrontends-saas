c8b9de3255bf725c884a820e5baab52cfe33ea0d
Jakub Juřena <jakub.jurena@koala42.com>
2025-01-27 12:36:07 +0100

    💚 fix marketing github action

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,8 +1,9 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packageJSON = require("../package.json");
 
 const commonConfig = require('./webpack.common');
+const packageJSON = require("../package.json");
 
 const devConfig = {
   mode: 'development',
@@ -20,6 +21,9 @@ const devConfig = {
         "./marketingBootstrap": "./src/bootstrap",
       },
       shared: packageJSON.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
